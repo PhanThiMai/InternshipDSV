693e5b9e5673b11341b8fc29a4c22152ac50fa6a
Phan Thi Mai <1612369@student.hcmus.edu.vn>
2019-10-26 14:50:04 +0700

    Update ProductCart.js

--- client/src/components/Products/Content/ProductsColumn/ProductCart/ProductCart.js
+++ client/src/components/Products/Content/ProductsColumn/ProductCart/ProductCart.js
@@ -10,25 +10,19 @@ class ProductCart extends React.Component {
         }
     }
 
-    handleClickProductCart = (e) => {
-        this.props.viewProductDetail(this.props.product.name);
-    }
-
     render() {
         const { product } = this.props;
-        const newUrl = product.name.replace(/\s/g, '');
+        const newUrl = product._id
 
         const soldOutItem = product.amount === 0 ? <div className="sold-out">Sold out</div> : <></>;
         return (
             <NavLink
                 to={`/products/${newUrl}`}
-                onClick={this.handleClickProductCart}
             >
                 <div className="cart-product" >
                     <img src={product.image} alt="product" className="product-img" />
                     {soldOutItem}
                     <p className="product-name">{product.name + ' ' + product.brand}</p>
-                    {/* <p className="product-brand">{product.brand}</p> */}
                     <p className="product-price">{`$${product.price}.00`}</p>
                 </div>
             </NavLink>
